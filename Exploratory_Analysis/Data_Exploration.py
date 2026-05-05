78335e59bc47a3c6ead1896d00c985ec42928714
RichieXYZ <114951607+RichieXYZ@users.noreply.github.com>
2026-05-05 14:39:06 +0200

    Add files via upload

---
+++ Exploratory_Analysis/Data_Exploration.py
@@ -0,0 +1,139 @@
+# Data Handling Libraries
+import pandas as pd
+
+# Visualization Libraries
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# File and Operating System Libraries
+import os
+
+# Warnings Management
+import warnings
+warnings.filterwarnings('ignore')
+
+# PATHS
+root_dir = "/Users/riccardo/PycharmProjects/Lung_Segmentation"
+data_dir = os.path.join(root_dir, "Data")
+img_dir  = os.path.join(data_dir, "ChestXRay/image")
+mask_dir = os.path.join(data_dir, "ChestXRay/mask")
+
+# Metadata .csv file
+metadata = pd.read_csv(os.path.join(data_dir, "MetaData.csv"))
+metadata.info()
+print(metadata.head())
+
+# --------------------------------------------------
+# Data analysis : positivity and geographical origin
+# --------------------------------------------------
+
+agg_data = metadata.ptb.value_counts()
+ct = pd.crosstab(metadata['gender'], metadata['ptb'])
+
+plt.figure(figsize=(12,5))
+plt.subplot(121)
+plt.pie(agg_data, labels=agg_data.index, autopct='%1.1f%%', startangle=90, colors=['darkorchid','tomato'])
+
+plt.subplot(122)
+bars = sns.countplot(metadata, x='ptb', hue='county', palette=['darkorchid','tomato'])
+plt.bar_label(bars.containers[0], label_type='center')
+plt.bar_label(bars.containers[1], label_type='center')
+plt.yticks([])
+plt.xticks([0,1],labels=['Negative','Positive'])
+plt.xlabel('Tuberculosis')
+plt.box(False)
+
+plt.suptitle('PTB Analysis',fontsize=16)
+plt.tight_layout()
+plt.show()
+
+# --------------------------------------------------
+# Data : Gender Analysis
+# --------------------------------------------------
+
+gender_mapping = {
+    'm': 'male', 'Male,': 'male', 'male35yrs': 'male', 'male,':'male',
+    'f': 'female', 'femal': 'female', 'female24yrs':'female','o':'female','female,':'female'
+}
+metadata.gender = metadata.gender.str.lower()
+metadata.gender.replace(gender_mapping,inplace=True)
+agg_data = metadata.gender.value_counts()
+ct = pd.crosstab(metadata['gender'], metadata['ptb'])
+
+plt.figure(figsize=(14,10))
+plt.subplot(221)
+plt.pie(agg_data,labels=agg_data.index,autopct='%1.1f%%', startangle=90,colors=['darkorchid','tomato'])
+
+plt.subplot(222)
+bars = plt.bar(agg_data.index,agg_data,color=['darkorchid','tomato'])
+plt.bar_label(bars, labels=[str(height) for height in agg_data],label_type='center')
+plt.yticks([])
+plt.box(False)
+
+plt.subplot(223)
+bars = ct.T.plot(kind='bar', color=['tomato', 'darkorchid'],ax=plt.gca())
+plt.bar_label(bars.containers[0], label_type='center')
+plt.bar_label(bars.containers[1], label_type='center')
+plt.xlabel('Tuberculosis')
+plt.yticks([])
+plt.box(False)
+
+plt.subplot(224)
+sns.heatmap(ct,annot=True,cbar=False,fmt='d',cmap='viridis')
+
+plt.suptitle('Gender Analysis',fontsize=16)
+plt.tight_layout()
+plt.show()
+
+# --------------------------------------------------
+# Data : Age Analysis
+# --------------------------------------------------
+
+metadata.age = (metadata.age
+                .replace({'16month':'1','39yr':'39','1yr':'1','male35':'35','female24':'24'})
+                .astype(int))
+custom_palette = ['darkorchid','tomato']
+
+plt.figure(figsize=(14,15))
+plt.subplot(321)
+plt.hist(metadata.age,color='darkorchid')
+plt.box(False)
+
+plt.subplot(322)
+plt.boxplot(metadata.age,vert=False,showmeans=True)
+plt.box(False)
+
+plt.subplot(323)
+sns.histplot(metadata,x='age',hue='gender',palette=custom_palette)
+plt.box(False)
+
+plt.subplot(324)
+sns.boxplot(metadata,x='age',y='gender',palette=custom_palette)
+plt.box(False)
+
+plt.subplot(325)
+sns.histplot(metadata,x='age',hue='ptb',palette=custom_palette)
+plt.box(False)
+
+plt.subplot(326)
+sns.boxplot(data=metadata, x='gender', y='age', hue='ptb', palette=custom_palette)
+plt.box(False)
+
+plt.suptitle('Age Analysis',fontsize=16)
+plt.tight_layout()
+plt.show()
+
+# --------------------------------------------------
+# FacetGrid of Age by Gender and PTB
+# --------------------------------------------------
+
+plt.figure(figsize=(14,8))
+g = sns.FacetGrid(metadata, col='ptb', row='gender', margin_titles=True,aspect=1.75)
+g.map(sns.histplot, 'age', bins=10, kde=True)
+
+# Adding titles and labels
+g.set_axis_labels('Age', 'Count')
+g.set_titles(col_template="{col_name} PTB", row_template="{row_name} Gender")
+
+# Show the plot
+plt.show()
---
+++ Exploratory_Analysis/Data_Visualization.py
@@ -0,0 +1,90 @@
+# Image Processing Libraries
+import cv2
+from cv2 import imread,resize
+from scipy.ndimage import label, find_objects
+
+# Data Handling Libraries
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# Visualization Libraries
+import matplotlib.pyplot as plt
+import seaborn as sns
+from tqdm import tqdm
+
+# File and Operating System Libraries
+import os
+
+# Warnings Management
+import warnings
+warnings.filterwarnings('ignore')
+
+# PATHS
+root_dir = "/Users/riccardo/PycharmProjects/Lung_Segmentation"
+data_dir = os.path.join(root_dir, "Data")
+img_dir  = os.path.join(data_dir, "ChestXRay/image")
+mask_dir = os.path.join(data_dir, "ChestXRay/mask")
+
+# Metadata .csv file
+metadata = pd.read_csv(os.path.join(data_dir, "MetaData.csv"))
+metadata.info()
+print(metadata.head())
+
+def get_colored_mask(image, mask_image, color=[255, 20, 255]):
+    mask_image_gray = cv2.cvtColor(mask_image, cv2.COLOR_BGR2GRAY)
+    mask = cv2.bitwise_and(mask_image, mask_image, mask=mask_image_gray)
+    mask_coord = np.where(mask != [0, 0, 0])
+    mask[mask_coord[0], mask_coord[1], :] = color
+    ret = cv2.addWeighted(image, 0.6, mask, 0.4, 0)
+    return ret
+
+# Plot some images examples
+filenames = next(os.walk(img_dir))[2][:50]
+print(filenames)
+for file in filenames:
+    img = imread(os.path.join(img_dir, file))
+    msk = imread(os.path.join(mask_dir, file))
+
+    plt.figure(figsize=(15, 5))
+
+    plt.subplot(131)
+    plt.title("Original Image")
+    plt.imshow(img)
+    plt.yticks([])
+    plt.xticks([])
+    plt.box(False)
+
+    plt.subplot(132)
+    plt.title("Mask")
+    plt.imshow(msk, cmap='binary_r')
+    plt.yticks([])
+    plt.xticks([])
+    plt.box(False)
+
+    plt.subplot(133)
+    plt.title("Image + Mask")
+    plt.imshow(get_colored_mask(img, msk))
+    plt.yticks([])
+    plt.xticks([])
+    plt.box(False)
+
+    plt.tight_layout()
+    plt.show()
+
+# Plot mask distribution heatmap
+filenames = next(os.walk(img_dir))[2]
+heatmap = np.zeros((1024, 1024))
+for file in tqdm(filenames, total=len(filenames)):
+    msk = imread(os.path.join(mask_dir, file), 0)
+    msk = resize(msk, (1024, 1024))
+    heatmap += msk
+heatmap /= len(filenames)
+
+plt.figure(figsize=(12, 12))
+
+plt.imshow(heatmap, cmap='coolwarm')
+plt.yticks([])
+plt.xticks([])
+
+plt.show()
